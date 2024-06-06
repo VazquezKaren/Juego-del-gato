ff06a82de263b91cc303d3f6df303d049824ebe3
VazquezKaren <169114492+VazquezKaren@users.noreply.github.com>
2024-06-06 14:32:43 -0600

    uo\p

---
+++ JuegoGato.py
@@ -0,0 +1,222 @@
+import turtle 
+import sys
+
+screen=turtle.Screen()
+draw=turtle.Turtle()
+screen.bgcolor("lightblue")
+jugadas_o=[]
+jugadas_x=[]
+jugadas_ambos=[]
+posicion=[]
+terminar=False
+reiniciar=True
+
+
+def maintitulo(texto,px,py,tam):
+    draw.penup()
+    draw.goto(px, py)
+    draw.color("orange")
+    draw.write(texto, align="center", font=("Baloo Bhaijaan", tam, "bold"))
+    draw.pendown()
+
+#titulo
+ventanaan = screen.window_width()
+ventanaA = screen.window_height()
+maintitulo("TicTacToc", 0, ventanaA//2 -150, 100)
+
+def tituloganaste(texto,px,py,tam):
+    draw.penup()
+    draw.goto(px, py)
+    draw.color("red")
+    draw.write(texto, align="center", font=("arial", tam, "bold"))
+    draw.pendown()
+
+
+def punto (px,py):
+    draw.speed(100)
+
+    draw.speed(60)
+    draw.penup()
+    draw.goto(px,py)  
+    draw.color("blue")
+    draw.pendown()
+    draw.dot(8)
+        
+punto(-100, 100)
+punto(0, 100)
+punto(100, 100)
+punto(-100, 0)
+punto(0, 0)
+punto(100, 0)
+punto(-100, -100)
+punto(0, -100)
+punto(100, -100)
+
+
+
+def cuadrado(px,py,cuadrados,tam):
+    draw.penup()
+    draw.goto(px,py)
+    draw.color("green")
+    draw.pendown()
+    draw.speed(60)
+    draw.pensize(6)
+
+    for x in range(cuadrados):
+        for y in range(4):
+            draw.forward(tam)
+            draw.right(90)
+        draw.penup()
+        draw.goto(px, py + (x + 1) * tam)
+        draw.pendown()
+
+cuadrado(50, -50, 3, 100)
+cuadrado(-50, -50, 3, 100)
+cuadrado(-150, -50, 3, 100)
+
+def Circulo (px, py, radio=35):
+    draw.penup()
+    draw.goto(px, py - radio)  
+    draw.color("blue")
+    draw.pendown()
+    draw.circle(radio)
+
+
+def cruz(px, py, tam=25):
+   
+    draw.penup()
+    draw.goto(px - tam, py + tam)  
+    draw.pendown()
+    draw.color("red")
+    draw.goto(px + tam, py - tam)  
+    draw.penup()
+    draw.goto(px - tam, py - tam)  
+    draw.pendown()
+    draw.goto(px + tam, py + tam)  
+
+def boton(px, py, tam,tam2):
+    draw = turtle.Turtle()
+    draw.penup()
+    draw.goto(px, py)
+    draw.color("black")
+    draw.pendown()
+    draw.speed(60)
+    draw.pensize(6)
+    
+    for o in range(2):  
+        draw.forward(tam)
+        draw.right(90)
+        draw.forward(tam2)
+        draw.right(90)
+
+
+boton(-250, -250, 150, 70)
+
+
+    
+
+turno = 0
+
+def click(x, y):
+    global turno, terminar
+    if terminar:
+        return
+    for (px,py) in positions:
+        if abs(x -px)<20  and abs(y -py) < 20:
+            if (px, py) in jugadas_ambos:
+                print("Este cuadro ya está ocupado")
+                return
+            while turno<9:
+                
+
+
+                if turno % 2 == 0:
+                    Circulo(px,py)
+                    jugadas_o.append((px,py))
+                    jugadas_ambos.append((px,py))
+                    posicion.append((px,py))
+                    print('jugada de jugador o :', jugadas_o)
+                    print("posiciones ocupadas",jugadas_ambos)
+                    
+                    if verificar(jugadas_o):
+                        print('Circulo Gana!!!!')
+                        tituloganaste("Circulo Gana!!!!", 0, screen.window_height() // 2 -600, 50)
+                        terminar=True
+                        print("Desea reiniciar el juego?")
+                        return
+                        
+                    
+                        
+                        
+                   
+                else:
+                    cruz(px, py)
+                    jugadas_x.append((px,py))
+                    jugadas_ambos.append((px,py))
+                    posicion.append((px,py))
+
+                    print('jugada de jugador x :', jugadas_x)
+                    print("posiciones ocupadas",jugadas_ambos)
+                    
+                    
+                    if verificar(jugadas_x):
+                        print("ganaste")
+                        tituloganaste("X Gana!!!!", 0, screen.window_height() // 2 -600, 50)
+                        terminar=True
+                        return
+                    
+                               
+                turno += 1
+                return
+
+
+jugadas_x=[]
+jugadas_ambos=[]
+
+positions = [
+    (-100, 100), (0, 100), (100, 100),
+    (-100, 0), (0, 0), (100, 0),
+    (-100, -100), (0, -100), (100, -100)
+]
+
+jugadas_ganadoras=[[(-100,100),(0,100),(100,100)],
+                   [(-100,0),(0,0),(0,-100)],
+                   [(-100,-100),(0,-100),(100,-100)],
+                   [(-100,100),(-100,0),(-100,-100)],
+                   [(0,100),(0,0),(0,-100)],
+                   [(100,100),(100,0),(100,-100)],
+                   [(-100,100),(0,0),(100,-100)],
+                   [(100,100),(0,0),(-100,-100)]
+]
+
+
+def verificar(jugadas):
+    for combinacion in jugadas_ganadoras:
+        if all(pos in jugadas for pos in combinacion):
+            return True
+    return False
+
+def no_duplicados(ambos):
+    for i in positions:
+        if all(linea in ambos for linea in i):
+            return True
+    return False
+
+
+
+
+screen.onclick(click)
+turtle.done()
+
+
+
+
+
+
+
+
+    
+
+
+
+
